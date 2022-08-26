07b6e7ce368b3065f8b695c5dec873e5f8c3839f
Matthew Setter <matthew@matthewsetter.com>
2022-08-26 13:37:16 +0200

    Format the retrieved information and render in a table
    This change basically pretties up the retrieved information so that it's
    that much easier to read. It renders the information in a table, and
    formats the start date, end date, and pricing information.

--- go.mod
+++ go.mod
@@ -3,11 +3,18 @@ module github.com/settermjd/twilio-usage-records
 go 1.18
 
 require (
+	github.com/bojanz/currency v1.0.4 // indirect
+	github.com/cockroachdb/apd/v3 v3.1.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible // indirect
+	github.com/jedib0t/go-pretty/v6 v6.3.7 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twilio/twilio-go v0.26.0 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- twilio-usage-records.go
+++ twilio-usage-records.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/bojanz/currency"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/joho/godotenv"
 	"github.com/twilio/twilio-go"
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
 func main() {
-	params := &openapi.ListUsageRecordParams{}
+	recordLimit := 20
+	params := &openapi.ListUsageRecordParams{Limit: &recordLimit}
 
 	err := godotenv.Load()
 	if err != nil {
@@ -26,15 +31,40 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"Start Date", "End Date", "Category", "Price"})
+
 		for _, record := range usageRecords {
-			fmt.Printf(
-				"%s,%s,%s,%f,%s\n",
-				*record.StartDate,
-				*record.EndDate,
+			price := fmt.Sprintf("%.2f", *record.Price)
+			amount, err := currency.NewAmount(price, strings.ToUpper(*record.PriceUnit))
+			if err != nil {
+				fmt.Println("Could not create currency amount from usage price.")
+			}
+			locale := currency.NewLocale("en_US")
+			formatter := currency.NewFormatter(locale)
+
+			startDate, err := time.Parse("2006-01-02", *record.StartDate)
+			if err != nil {
+				fmt.Println("Could not format start date.")
+			}
+
+			endDate, err := time.Parse("2006-01-02", *record.EndDate)
+			if err != nil {
+				fmt.Println("Could not format end date.")
+			}
+
+			t.AppendRow([]interface{}{
+				startDate.Format("January 2, 2006"),
+				endDate.Format("January 2, 2006"),
 				*record.Category,
-				*record.Price,
-				*record.PriceUnit,
-			)
+				formatter.Format(amount),
+			})
 		}
+
+		t.AppendSeparator()
+		t.AppendFooter(table.Row{"Total records: ", len(usageRecords)}, table.RowConfig{AutoMerge: true})
+
+		t.Render()
 	}
 }
