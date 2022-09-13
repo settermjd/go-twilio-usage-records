098792f9cb267cc186ad0ec1fa793bf618a743b8
Matthew Setter <matthew@matthewsetter.com>
2022-09-13 13:07:34 +0200

    Refactor the code to be more maintainable
    The intention of this change is to, as the description says, make the
    code more maintainable.
    
    As it was, it did the job that was required. However, everything was all
    dumped in the main function. From what I've been learning about
    idiomatic Go (and have learned over the years writing software) that's a
    bad thing to do.
    
    So, this change reformats it, breaking the large main function up into
    a series of structs and functions, ones that could be further
    restructured in a custom module, if desired.

--- twilio-usage-records.go
+++ twilio-usage-records.go
@@ -1,3 +1,13 @@
+// Copyright © 2022 Matthew Setter <matthew@matthewsetter.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+// Twilio Usage Records is a small application that supports the
+// retrieval of usage records from a Twilio account and renders
+// them in a variety of ways.
+// At the moment, only tabular format is supported.
+
 package main
 
 import (
@@ -9,62 +19,105 @@ import (
 
 	"github.com/bojanz/currency"
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"github.com/twilio/twilio-go"
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func main() {
-	recordLimit := 20
-	params := &openapi.ListUsageRecordParams{Limit: &recordLimit}
+// TwilioUsageFormatOptions provides formatting options for
+// TwilioUsageTableFormatter and other TwilioUsage*Formatter objects.
+type TwilioUsageFormatOptions struct {
+	OutputDateFormat string
+	InputDateFormat  string
+}
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+// TwilioUsageTableFormatter formats a list of TwilioUsage records in tabular
+// format.
+type TwilioUsageTableFormatter struct {
+	options TwilioUsageFormatOptions
+}
 
-	client := twilio.NewRestClientWithParams(twilio.ClientParams{
-		Username: os.Getenv("TWILIO_ACCOUNT_SID"),
-		Password: os.Getenv("TWILIO_AUTH_TOKEN"),
-	})
-	usageRecords, err := client.Api.ListUsageRecord(params)
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		t := table.NewWriter()
-		t.SetOutputMirror(os.Stdout)
-		t.AppendHeader(table.Row{"Start Date", "End Date", "Category", "Price"})
+// FormatRecords takes an array of Twilio usage records and renders them in a table.
+// While there are a number of properties available on a ApiV2010UsageRecord object,
+// the function only renders the start and end dates, category, and price in the
+// functions output. At some point in the future, this could be changed, but for
+// this, small, example, I feel that these four columns suffice3.
+func (r *TwilioUsageTableFormatter) FormatRecords(usageRecords []openapi.ApiV2010UsageRecord) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Start Date", "End Date", "Category", "Price"})
 
-		for _, record := range usageRecords {
-			price := fmt.Sprintf("%.2f", *record.Price)
-			amount, err := currency.NewAmount(price, strings.ToUpper(*record.PriceUnit))
-			if err != nil {
-				fmt.Println("Could not create currency amount from usage price.")
-			}
-			locale := currency.NewLocale("en_US")
-			formatter := currency.NewFormatter(locale)
+	for _, record := range usageRecords {
+		price := fmt.Sprintf("%.2f", *record.Price)
+		amount, err := currency.NewAmount(price, strings.ToUpper(*record.PriceUnit))
+		if err != nil {
+			log.Println("Could not create currency amount from usage price.")
+			continue
+		}
+		locale := currency.NewLocale("en_US")
+		formatter := currency.NewFormatter(locale)
 
-			startDate, err := time.Parse("2006-01-02", *record.StartDate)
-			if err != nil {
-				fmt.Println("Could not format start date.")
-			}
-
-			endDate, err := time.Parse("2006-01-02", *record.EndDate)
-			if err != nil {
-				fmt.Println("Could not format end date.")
-			}
-
-			t.AppendRow([]interface{}{
-				startDate.Format("January 2, 2006"),
-				endDate.Format("January 2, 2006"),
-				*record.Category,
-				formatter.Format(amount),
-			})
+		startDate, err := time.Parse(r.options.InputDateFormat, *record.StartDate)
+		if err != nil {
+			log.Println("Could not format start date.")
+			continue
 		}
 
-		t.AppendSeparator()
-		t.AppendFooter(table.Row{"Total records: ", len(usageRecords)}, table.RowConfig{AutoMerge: true})
+		endDate, err := time.Parse(r.options.InputDateFormat, *record.EndDate)
+		if err != nil {
+			log.Println("Could not format end date.")
+			continue
+		}
 
-		t.Render()
+		t.AppendRow([]interface{}{
+			startDate.Format(r.options.OutputDateFormat),
+			endDate.Format(r.options.OutputDateFormat),
+			*record.Category,
+			formatter.Format(amount),
+		})
 	}
+
+	t.AppendSeparator()
+	t.AppendFooter(
+		table.Row{"Total records: ", len(usageRecords)},
+	)
+
+	t.Render()
+}
+
+func main() {
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	// Add this as a value on a struct
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: viper.GetString("TWILIO_ACCOUNT_SID"),
+		Password: viper.GetString("TWILIO_AUTH_TOKEN"),
+	})
+
+	//maxRecords := flag.Int("n", 20, "Maximum number of records to return")
+	//startDate := flag.String("s", "", "The earliest date of a usage record")
+	//endDate := flag.String("e", "", "The latest date of a usage record")
+
+	recordLimit := viper.GetInt("RECORD_LIMIT")
+	params := &openapi.ListUsageRecordParams{
+		Limit: &recordLimit,
+	}
+	usageRecords, err := client.Api.ListUsageRecord(params)
+	if err != nil {
+		panic(fmt.Errorf("Could not retrieve usage records: %w", err))
+	}
+
+	recordFormatter := TwilioUsageTableFormatter{
+		TwilioUsageFormatOptions{
+			viper.GetString("OUTPUT_DATE_FORMAT"),
+			viper.GetString("INPUT_DATE_FORMAT"),
+		},
+	}
+	recordFormatter.FormatRecords(usageRecords)
 }
